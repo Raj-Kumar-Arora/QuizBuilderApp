a58a210b6a046e9deb9951d0c747c747ea2b35ef
Raj Kumar Arora <Raj.Kumar.Arora@outlook.com>
2025-12-10 23:17:45 +0100

    [BACK-END] : Adding Models metadata and Quiz Controller skeleton

---
+++ BackEnd/WebAPI/Models/Author.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPI.Models
+{
+    public class Author
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; } = string.Empty;
+    }
+}
---
+++ BackEnd/WebAPI/Models/Quiz.cs
@@ -0,0 +1,61 @@
+﻿namespace WebAPI.Models
+{
+    public class Quiz
+    {
+        public Quiz(int id, string title, int authorId)
+        {
+            Id = id;
+            Title = title;
+            AuthorId = authorId;
+        }
+
+        #region Properties Region
+        public int Id { get; init; }
+
+        public required string Title { get; set; } = string.Empty;
+        public required int AuthorId { get; init; }
+        public IReadOnlyCollection<Question> Questions => _questions.AsReadOnly();
+
+        #endregion Properties Region
+
+
+
+        #region Questions Region 
+        private const int MinQuestions = 1;
+        private const int MaxQuestions = 10;
+
+        private readonly List<Question> _questions = new();
+
+        // IReadOnlyCollection => read-only view of a collection
+        //  * Exposes only:  Count / foreach / Index reading
+        //  * Does not expose: Add / Remove / Clear / Index writing
+
+
+        public void AddQuestion(Question question)
+        {
+            if (_questions.Count >= MaxQuestions)
+                throw new InvalidOperationException("A quiz can have a maximum of 10 questions.");
+
+            _questions.Add(question);
+        }
+
+        public void RemoveQuestion(Question question)
+        {
+            ArgumentNullException.ThrowIfNull(question);
+
+            _questions.Remove(question);
+        }
+
+        // Validate that the quiz meets all requirements
+        // Will be called before saving the quiz to DB
+        public void Validate()
+        {
+            if (_questions.Count < MinQuestions)
+                throw new InvalidOperationException("A quiz must have at least 1 question.");
+        }
+
+        #endregion Questions Region 
+
+
+    }
+}
