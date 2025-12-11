ec7633d4a701f9bf86405a39ca18c0fbc889e0f3
Raj Kumar Arora <Raj.Kumar.Arora@outlook.com>
2025-12-11 22:56:55 +0100

    [BACK-END] : Updated DTOs for User

--- BackEnd/WebAPI/DTOs/User/UserCreateRequest.cs
+++ BackEnd/WebAPI/DTOs/User/UserCreateRequest.cs
@@ -1,6 +1,9 @@
 ﻿namespace WebAPI.DTOs.User
 {
     public class UserCreateRequest
-    {
+    { 
+        public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
     }
 }
